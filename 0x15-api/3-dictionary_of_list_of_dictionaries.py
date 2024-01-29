89fbd97d85ce9351de5a1af782194eb189ca1d0b
Leul <leulassefa255@gmail.com>
2024-01-29 02:46:24 -0800

    API

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
     with open("todo_all_employees.json", "w") as jsonfile:
         json.dump({
             user.get("id"): [{
+                "username": todo.get("username"),
                 "task": todo.get("title"),
                 "completed": todo.get("completed"),
                 "username": user.get("username")
