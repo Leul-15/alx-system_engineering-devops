a83e5830cd1107182058f4c8dd064beac1be2678
Leul <leulassefa255@gmail.com>
2024-02-13 10:30:49 -0800

    API advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,19 +4,14 @@ Query the Reddit API and returns the number of subscribers
 """
 import requests
 
+
 def number_of_subscribers(subreddit):
     """
     Get the number of subscribers
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'linux User Agent 1.0'}
-    try:
-        response = requests.get(url, headers=headers)
-        data = response.json()
-        if 'data' in data and 'subscribers' in data['data']:
-            return data['data']['subscribers']
-        else:
-            return 0
-    except Exception as e:
-        print("Error:", e)
+    headers = {"User-Agent": "linux User Agent 1.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
         return 0
+    return response.json().get("data").get("subscribers")
