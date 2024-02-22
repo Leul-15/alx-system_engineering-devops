7bb1b7b3dab21b5e9cc2e6706a93677f0a27689f
Leul <leulassefa255@gmail.com>
2024-02-22 08:44:08 -0800

    Web stack debugging

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,11 @@
 # Increases the amount of traffic an Nginx server can handle
 
-exec { 'fix_error':
-  command => "/bin/sed -i 's/15/4096/g' /etc/default/nginx; ; sudo service nginx restart",
+exec { 'increse-limit':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+-> exec { 'restart-nginx':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
